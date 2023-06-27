993040144b444e64165a1a93ff7ff60d343bf2d1
AdelBoussa <aboussenane@gmail.com>
2023-06-26 21:43:06 -0700

    Add code to prevent page refresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+
 import Clock from './Clock';
 import Button from './Button';
 
 import './App.css';
 
-function App() {
+import React, { useEffect } from 'react';
+
+const App = () => {
+  useEffect(() => {
+    const unloadHandler = (event) => {
+      event.preventDefault();
+      event.returnValue = "Are you sure you want to refresh? Data will be lost.";
+    };
+
+    window.addEventListener('beforeunload', unloadHandler);
+
+    return () => {
+      window.removeEventListener('beforeunload', unloadHandler);
+    };
+  }, []);
   return (
     
     <div className="App">
